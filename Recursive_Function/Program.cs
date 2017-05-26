36dfbbfa996f449c1ac483b81165cf29464fde41
atma0004 <atma0004@edu.eal.dk>
2017-05-26 17:03:05 +0200

    Add project files.

---
+++ Recursive_Function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursive_Function
+{
+    class Program
+    {
+        //Recursion is when a method call it selfe 
+        static void Main(string[] args)
+        {
+            int answer = 0;
+            answer = factorial(5);
+            Console.WriteLine(answer);
+        }
+        private static int factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * factorial(n - 1);
+            }
+        }
+
+    }
+}
